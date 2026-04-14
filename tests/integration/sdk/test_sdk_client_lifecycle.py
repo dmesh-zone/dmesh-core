9ef95cfda8e3180b118ff9fbb25e5a4a28390648
Joao Vicente <joao.diogo.vicente@gmail.com>
2026-04-14 23:31:49 +0100

    client lifecycle test initial

--- tests/integration/sdk/test_sdk_client_lifecycle.py
+++ tests/integration/sdk/test_sdk_client_lifecycle.py
@@ -1,3 +1,4 @@
+from dmesh.sdk.core.id_generator import make_dp_id
 from datetime import datetime
 import pytest
 from uuid import UUID
@@ -67,61 +68,96 @@ def dc_repo(factory):
 
 @pytest.mark.skip(reason="Not implemented yet")
 @pytest.mark.asyncio
-async def test_sdk_client_full_lifecycle(sdk):
-    """Simulates the full lifecycle of a dmesh-sdk client. 
+async def test_sdk_cicd_client(sdk):
+    """Simulates the CI/CD client usage of dmesh-sdk. 
     Assuming data product spec is minimalist containing only domain, name and outputPorts
-    Assumes other information will be collected from other sources"""
-    # TODO: (later) Consider how to delete data product and data contracts that have been decommissioned
-    # Step 1. data product spec is provided with with minimal input provided: domain, finance and outputPorts
-    dp_spec_input = {"domain": "finance", "name": "ledger", "outputPorts": [{"name": "ledger"}, {"name": "transactions"}]}
-    # The client must infer the version and status of the data product, as these are required by the spec schema
-    dp_spec_enriched = dp_spec_input.copy()
-    # Set defaults for version and status if not provided
-    if "version" not in dp_spec_enriched:
-        dp_spec_enriched["version"] = "v1.0.0"
-    if "status" not in dp_spec_enriched:
-        dp_spec_enriched["status"] = "active"
-    # Step 2: Get additional information from data product onboarding information
-    # dp_onboarding_info = client.sdk.get_data_product_onboarding_information(dp_spec_input["domain"], dp_spec_input["name"])
-    dp_onboarding_info = {
-        "data_product_business_name": "",
-        "data_product_technical_name": "",
-        "platform_data_product_id": "123e4567-e89b-12d3-a456-426614174000",
-        "domain_data_product_id": "0001",
-        "schema_name": "",
-        "data_engineer_group_name": "",
-        "data_analyst_group_name": "",
-        "consumer_applications_group_name": "",
-        "service_account_group_name": ""
-    }
-    # TODO: (later) Step x: test retrived id conforms with deterministic dp id algorithm
-    # TODO: Step x: Check if existing data product and data contracts
-    # TODO: Step x: Create data product and data contracts if they don't exist
-        # TODO: Step x.x: set dp id
-        # TODO: Step x.x: set dp dataProductTier (sourceAligned)
-        # TODO: Step x.x: set dp technology (databricks)
-        # TODO: Step x.x: set dp domainDataProductId (0001)
-        # TODO: Step x.x: create output port contractId
-        # TODO: Step x.x: Create data product with all information (output port contract ids added later)
-        # TODO: Step x.x: Prepare data contract with main server info including host, catalog and schema
-        # TODO: Step x.x: Prepare data contract with roles including role (name) and acccess (read/write)
-        # TODO: Step x.x: Prepare data contract with port adapter server info (e.g. location)
-        # TODO: Step x.x: Create dataSource data product for data source (using sadp.dataSourceType as hint for source dp.technoloy) - Consider deterministic dp id input for dataSource data product
-        # TODO: Step x.x: Create Data Usage Agreement Specification for source data product (to capture source dp to sadp connection)
-        # TODO: Step x.x: Create application data product for applications (using curated_discover_and_observe.applications - derived from landing_iam.groups_and_members) - Consider deterministic dp id input for application data product
-        # TODO: Step x.x: Create Data Usage Agreement Specification for application data product (to capture dp to application connections)
-    # TODO: Step x: Update data product and data contracts if they exist
-        # TODO: Step x.x: Check correct dp id
-        # TODO: Step x.x: Check correct dp dataProductTier (sourceAligned)
-        # TODO: Step x.x: Check correct dp technology (databricks)
-        # TODO: Step x.x: Check correct dp domainDataProductId (0001)
-        # TODO: Step x.x: Check correct output port contractId
-        # TODO: Step x.x: Check data product has all information (output port contract ids added later)
-        # TODO: Step x.x: Check data contract has main server info including host, catalog and schema
-        # TODO: Step x.x: Check data contract has all roles including role (name) and acccess (read/write)
-        # TODO: Step x.x: Check data contract has all port adapter server info (e.g. location)
-        # TODO: Step x.x: Check dataSource data product for data source (using sadp.dataSourceType as hint for source dp.technoloy)
-        # TODO: Step x.x: Check Data Usage Agreement Specification for source data product (to capture source dp to sadp connection)
-        # TODO: Step x.x: Check application data product for applications (using curated_discover_and_observe.applications - derived from landing_iam.groups_and_members) - Consider deterministic dp id input for application data product
-        # TODO: Step x.x: Check Data Usage Agreement Specification for application data product (to capture dp to application connections)
-    # TODO: Step x: Create or Update schema for each output port (will execute periodically - unless dp.schemaAutoDiscovery:disabled)
+    Assumes other information will be collected from other sources: e.g. repository_info and onboarding_info"""
+    # TODO: SDK to populate dpSpec outputPorts contractId and contractVersion
+    DOMAIN = "finance"
+    DP1_BUSINESS_NAME = "SAP FI"
+    DP1_TECHNICAL_NAME = "sap_fi"
+    DP1_SCHEMA_NAME = "landing_sap_fi"
+    DP1_TABLE1_NAME = "accounting_document_line_items"
+    DP1_DATA_SOURCE_TECH = "sap"
+    DP1_DOMAIN_DP_ID = "0001"
+    DP2_BUSINESS_NAME = "Account Receivables Ledger"
+    DP2_TECHNICAL_NAME = "account_receivables_ledger"
+    DP2_SCHEMA_NAME = "curated_account_receivables_ledger"
+    DP1_TABLE2_NAME = "customer_open_items"
+    DP2_DOMAIN_DP_ID = "0002"
+    # Step 1-3: domain_data_product_repository_scan() 
+    # repository_info = domain_data_product_repository_scan() 
+    domain_repository_info = [
+        {
+            "domain": DOMAIN,
+            "name": DP1_TECHNICAL_NAME, 
+            "spec": { 
+                "outputPorts": [ { "name": DP1_TABLE1_NAME}],
+                "customProperties": [
+                    { "property": "dataProductTier", "value": "sourceAligned" },
+                    { "property": "dataSourceTechnology", "value": DP1_DATA_SOURCE_TECH}
+                ]
+            }
+        },
+        {
+            "domain": DOMAIN,
+            "name": DP2_TECHNICAL_NAME, 
+            "spec": { 
+                "outputPorts": [ { "name": DP1_TABLE2_NAME} ],
+                "customProperties": [
+                    { "property": "dataProductTier", "value": "curated" },
+                    { "property": "portAdapters", "value": ["odata", "iceberg"] }
+                ]
+            }
+        }
+    ]
+    # Step 4-5: Get data_product_onboarding information 
+    dp_onboarding_info = [
+        {
+            "domain": DOMAIN,
+            "data_product_business_name": DP1_BUSINESS_NAME,
+            "data_product_technical_name": DP1_TECHNICAL_NAME,
+            "platform_data_product_id": sdk.id_generator.make_dp_id({"domain": DP1_DOMAIN_DP_ID, "name": DP1_TECHNICAL_NAME}),
+            "domain_data_product_id": DP1_DOMAIN_DP_ID,
+            "schema_name": DP1_SCHEMA_NAME,
+            "data_engineer_group_name": f"{DOMAIN.upper()}_DATA_ENGINEER_{DP1_DOMAIN_DP_ID}",
+            "data_analyst_group_name": f"{DOMAIN.upper()}_DATA_ANALYST_{DP1_DOMAIN_DP_ID}",
+            "consumer_applications_group_name": f"{DOMAIN.upper()}_CONSUMER_APPLICATIONS_{DP1_DOMAIN_DP_ID}",
+            "service_account_group_name": f"{DOMAIN.upper()}_SERVICE_ACCOUNT_{DP1_DOMAIN_DP_ID}"
+        },
+        {
+            "domain": DOMAIN,
+            "data_product_business_name": DP2_BUSINESS_NAME,
+            "data_product_technical_name": DP2_TECHNICAL_NAME,
+            "platform_data_product_id": sdk.id_generator.make_dp_id({"domain": DP2_DOMAIN_DP_ID, "name": DP2_TECHNICAL_NAME}),
+            "domain_data_product_id": DP2_DOMAIN_DP_ID,
+            "schema_name": DP2_SCHEMA_NAME,
+            "data_engineer_group_name": f"{DOMAIN.upper()}_DATA_ENGINEER_{DP2_DOMAIN_DP_ID}",
+            "data_analyst_group_name": f"{DOMAIN.upper()}_DATA_ANALYST_{DP2_DOMAIN_DP_ID}",
+            "consumer_applications_group_name": f"{DOMAIN.upper()}_CONSUMER_APPLICATIONS_{DP2_DOMAIN_DP_ID}",
+            "service_account_group_name": f"{DOMAIN.upper()}_SERVICE_ACCOUNT_{DP2_DOMAIN_DP_ID}"
+        }
+    ]
+    # Step 6: Combine repository and onboarding information
+    # For each data product in the repository, merge in onboarding information for matching domain and data product (technical) name
+    dp_spec_list = []
+    for dp in domain_repository_info:
+        for dp_onboarding in dp_onboarding_info:
+            if dp["domain"] == dp_onboarding["domain"] and dp["name"] == dp_onboarding["data_product_technical_name"]:
+                dp_spec_list.append({**dp, **dp_onboarding})
+
+    # Prepare data products and data contracts and update them in dmesh-db for each data product in repository
+    for dp_info in dp_spec_list:
+        # Step 7: Prepare data product spec
+        dp_spec = dp_info["spec"]
+        dp_spec["domain"] = dp_info["domain"]
+        dp_spec["name"] = dp_info["data_product_technical_name"]
+        # Enriching will populate default values for version and status, outputPort version and contractId, and portAdapter expansion
+        enchiched_dp_spec = await sdk.enrich_data_product_spec(dp_spec)
+        # Step 8: Create or update data product
+        upserted_dp = await sdk.put_data_product(enchiched_dp_spec)
+        # TODO: sdk.autoDataSourceDpCreationUponSourceAlignedDpCreation
+        # TODO: Step 11 Prepare Data Contract
+        # TODO: Add data contract roles
+        # TODO: Add data contract servers
+        # TODO: Upsert Data Contract
